57ecb269ce71ae74ff5abe69b92a6060b215c197
Kalle Alm <kalle.alm@gmail.com>
2014-06-12 18:09:39 +0200

    Missing license.

--- PDObC/NSArray+PDIXMPArchive.m
+++ PDObC/NSArray+PDIXMPArchive.m
@@ -1,9 +1,20 @@
 //
-//  NSArray+PDIXMPArchive.m
-//  PDF Link Inspector
+// NSArray+PDIXMPArchive.m
 //
-//  Created by Karl-Johan Alm on 28/05/14.
-//  Copyright (c) 2014 ReOrient Media. All rights reserved.
+// Copyright (c) 2014 Karl-Johan Alm (http://github.com/kallewoof)
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 #import "NSArray+PDIXMPArchive.h"
