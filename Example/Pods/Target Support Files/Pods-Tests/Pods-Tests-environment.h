98a17894e183d676e0c356ba85593bf65baeabe2
Kalle Alm <kalle.alm@gmail.com>
2014-11-15 11:54:13 +0900

    Pod update

--- Example/Pods/Target Support Files/Pods-Tests/Pods-Tests-environment.h
+++ Example/Pods/Target Support Files/Pods-Tests/Pods-Tests-environment.h
@@ -15,13 +15,13 @@
 // Expecta+Snapshots
 #define COCOAPODS_POD_AVAILABLE_Expecta_Snapshots
 #define COCOAPODS_VERSION_MAJOR_Expecta_Snapshots 1
-#define COCOAPODS_VERSION_MINOR_Expecta_Snapshots 2
-#define COCOAPODS_VERSION_PATCH_Expecta_Snapshots 1
+#define COCOAPODS_VERSION_MINOR_Expecta_Snapshots 3
+#define COCOAPODS_VERSION_PATCH_Expecta_Snapshots 0
 
 // FBSnapshotTestCase
 #define COCOAPODS_POD_AVAILABLE_FBSnapshotTestCase
 #define COCOAPODS_VERSION_MAJOR_FBSnapshotTestCase 1
-#define COCOAPODS_VERSION_MINOR_FBSnapshotTestCase 2
+#define COCOAPODS_VERSION_MINOR_FBSnapshotTestCase 4
 #define COCOAPODS_VERSION_PATCH_FBSnapshotTestCase 0
 
 // PajdegCore
@@ -34,7 +34,7 @@
 #define COCOAPODS_POD_AVAILABLE_PajdegPDF
 #define COCOAPODS_VERSION_MAJOR_PajdegPDF 0
 #define COCOAPODS_VERSION_MINOR_PajdegPDF 2
-#define COCOAPODS_VERSION_PATCH_PajdegPDF 0
+#define COCOAPODS_VERSION_PATCH_PajdegPDF 1
 
 // Specta
 #define COCOAPODS_POD_AVAILABLE_Specta
