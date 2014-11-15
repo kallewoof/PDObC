0b00fe521a63b7e52ee892a32f5d7970550eee71
Kalle Alm <kalle.alm@gmail.com>
2014-11-15 12:02:15 +0900

    Removed Expecta+Snapshots, because it is broken.

--- Example/Pods/Target Support Files/Pods-Tests/Pods-Tests-environment.h
+++ Example/Pods/Target Support Files/Pods-Tests/Pods-Tests-environment.h
@@ -12,12 +12,6 @@
 #define COCOAPODS_VERSION_MINOR_Expecta 3
 #define COCOAPODS_VERSION_PATCH_Expecta 1
 
-// Expecta+Snapshots
-#define COCOAPODS_POD_AVAILABLE_Expecta_Snapshots
-#define COCOAPODS_VERSION_MAJOR_Expecta_Snapshots 1
-#define COCOAPODS_VERSION_MINOR_Expecta_Snapshots 3
-#define COCOAPODS_VERSION_PATCH_Expecta_Snapshots 0
-
 // FBSnapshotTestCase
 #define COCOAPODS_POD_AVAILABLE_FBSnapshotTestCase
 #define COCOAPODS_VERSION_MAJOR_FBSnapshotTestCase 1
